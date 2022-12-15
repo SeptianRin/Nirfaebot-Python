540882c096e65e44ce8ff626223d9b7323b3d052
Septian Rinaldi Rianggoro <septian.rianggoro@ist.id>
2022-12-15 13:59:14 +0700

    add placeholder slash command

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ keep_alive()
 token = os.getenv("TOKEN")
 PREFIX = "ne "
 bot = hikari.GatewayBot(token=token, intents=hikari.Intents.ALL)
+COMMAND_GUILD_ID = hikari.UNDEFINED
 
 
 def is_command(cmd_name: str, content: str) -> bool:
@@ -78,6 +79,48 @@ async def message(event: hikari.GuildMessageCreateEvent) -> None:
           datetime.timezone.utc) + datetime.timedelta(seconds=10))
       await event.message.respond(hikari.Embed(title=f"hahaha"))
 
+@bot.listen()
+async def register_commands(event: hikari.StartingEvent) -> None:
+    """Register ping and info commands."""
+    application = await bot.rest.fetch_application()
+
+    commands = [
+        bot.rest.slash_command_builder("ping", "Get the bot's latency."),
+        bot.rest.slash_command_builder("info", "Learn something about the bot."),
+        bot.rest.slash_command_builder("ephemeral", "Send a very secret message."),
+    ]
+
+    await bot.rest.set_application_commands(
+        application=application.id,
+        commands=commands,
+        guild=COMMAND_GUILD_ID,
+    )
+
+@bot.listen()
+async def handle_interactions(event: hikari.InteractionCreateEvent) -> None:
+    """Listen for slash commands being executed."""
+    if not isinstance(event.interaction, hikari.CommandInteraction):
+        # only listen to command interactions, no others!
+        return
+
+    if event.interaction.command_name == "ping":
+        await event.interaction.create_initial_response(
+            hikari.ResponseType.MESSAGE_CREATE,
+            f"Pong! {bot.heartbeat_latency * 1_000:.0f}ms",
+        )
+
+    elif event.interaction.command_name == "info":
+        await event.interaction.create_initial_response(
+            hikari.ResponseType.MESSAGE_CREATE,
+            "Hello, this is an example bot written in hikari!",
+        )
+
+    elif event.interaction.command_name == "ephemeral":
+        await event.interaction.create_initial_response(
+            hikari.ResponseType.MESSAGE_CREATE,
+            "Only you can see this, keep it a secret :)",
+            flags=hikari.MessageFlag.EPHEMERAL,
+        )
 
 try:
   bot.run()
